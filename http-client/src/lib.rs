ba253e687e690987849422626ddc00105e095eb3
igorAAA <igor.devel@gmail.com>
2026-03-01 20:13:39 +0100

    an http client added; refactoring, code formating;

--- http-client/src/lib.rs
+++ http-client/src/lib.rs
@@ -2,19 +2,59 @@ pub mod config;
 pub mod error;
 
 use crate::config::WeatherApiConfig;
+use async_trait::async_trait;
 use error::Result;
+use log::debug;
+use serde::Deserialize;
 use url::Url;
 
-pub fn parse_url(config: WeatherApiConfig) -> Result<Url> {
-    let base_url = config.base_url;
+#[async_trait]
+pub trait HttpClient {
+    fn weather_response<Resp: for<'a> Deserialize<'a>>(
+        &self,
+    ) -> impl std::future::Future<Output = Result<Resp>>;
+}
 
-    let current_uri = format!("{}/current.json", base_url);
+pub struct WeatherClient {
+    client: reqwest::Client,
+    url: Url,
+}
 
-    let coords = config.coords.as_str();
+impl WeatherClient {
+    pub fn new(config: WeatherApiConfig) -> Result<Self> {
+        let client = reqwest::Client::builder().build()?;
+        let url = UrlParser.parse_url(config)?;
+        Ok(Self { client, url })
+    }
+}
 
-    let weather_api_key = config.weather_api_key.as_str();
+impl HttpClient for WeatherClient {
+    async fn weather_response<Resp: for<'a> Deserialize<'a>>(&self) -> Result<Resp> {
+        let resp = self.client.get(self.url.as_str()).send().await?;
+        debug!("Response status: {}", resp.status());
 
-    let params = vec![("key", weather_api_key), ("q", coords)];
-    let result = Url::parse_with_params(&current_uri, params)?;
-    Ok(result)
+        let text = resp.text().await?;
+        debug!("Response: {}", text);
+
+        let resp = serde_json::from_str::<Resp>(&text)?;
+        Ok(resp)
+    }
+}
+
+struct UrlParser;
+
+impl UrlParser {
+    fn parse_url(&self, config: WeatherApiConfig) -> Result<Url> {
+        let base_url = config.base_url;
+
+        let current_uri = format!("{}/current.json", base_url);
+
+        let coords = config.coords.as_str();
+
+        let weather_api_key = config.weather_api_key.as_str();
+
+        let params = vec![("key", weather_api_key), ("q", coords)];
+        let result = Url::parse_with_params(&current_uri, params)?;
+        Ok(result)
+    }
 }
